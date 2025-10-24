908304dc95e46822d50de6630a1570a72dba0c3d
yonihersh-code <yonibhersh@gmail.com>
2025-10-24 14:55:07 +1100

    Update vite.config.ts

--- cricket-calculator-final/vite.config.ts
+++ cricket-calculator-final/vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
+<content><![CDATA[import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+plugins: [react()],
 })
+]]></content>
