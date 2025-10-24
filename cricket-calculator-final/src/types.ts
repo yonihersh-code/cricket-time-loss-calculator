649d77f9b10fb73b9a2ce956479d9c89cccc0389
yonihersh-code <yonibhersh@gmail.com>
2025-10-24 15:29:52 +1100

    Update types.ts

--- cricket-calculator-final/src/types.ts
+++ cricket-calculator-final/src/types.ts
@@ -1,8 +1,7 @@
-<content><![CDATA[export interface MatchDetails {
-matchFormat: 'one-day' | 'two-day';
-oversPerTeam: string;
-timeLost: string;
-interruptionPoint: string;
-oversBowled: string;
+export interface MatchDetails {
+  matchFormat: 'one-day' | 'two-day';
+  oversPerTeam: string;
+  timeLost: string;
+  interruptionPoint: string;
+  oversBowled: string;
 }
-]]></content>
