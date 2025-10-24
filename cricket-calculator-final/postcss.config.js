a4f392d7f64cbf1e99f17fca6ea121eff02623f7
yonihersh-code <yonibhersh@gmail.com>
2025-10-24 15:24:51 +1100

    Update postcss.config.js

--- cricket-calculator-final/postcss.config.js
+++ cricket-calculator-final/postcss.config.js
@@ -1,8 +1,6 @@
-A supporting configuration file that Tailwind CSS needs to work correctly with the build process.
-<content><![CDATA[export default {
-plugins: {
-tailwindcss: {},
-autoprefixer: {},
-},
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 }
-]]></content>
